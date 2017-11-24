f19d15502dff04229ac4e6536a862bfe449b5f70
Patrick Sollars <pjsollars@gmail.com>
2017-11-24 18:49:45 -0500

    attempt to setup server api

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(express.static('client/build'));
+
+// event search
+//const FEBL_ACCESS_TOKEN = "815582698621963|M98FZpl1cTvP0STpQUvGpFbH2AQ";
+const EventSearch = require("facebook-events-by-location-core");
+const es = new EventSearch();
+
+es.search({
+  "lat": 42.9634,
+  "lng": -85.6681,
+  "accessToken": "815582698621963|M98FZpl1cTvP0STpQUvGpFbH2AQ"
+}).then(function (events) {
+    console.log(JSON.stringify(events));
+}).catch(function (error) {
+    console.error(JSON.stringify(error));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`JSON Server is running on ${PORT}`);
+});
