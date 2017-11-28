f76c83748dc25cb72c66b489bbb4e8e004156dbd
Patrick Sollars <pjsollars@gmail.com>
2017-11-28 13:59:10 -0500

    adds category filtering

--- client/src/components/EventSearch.js
+++ client/src/components/EventSearch.js
@@ -18,9 +18,35 @@ class EventSearch extends Component {
       <div className="EventSearch">
         <input type="text" placeholder="location" onChange={this.handleLocation} />
         <button onClick={this.handleSubmit}>Fetch Events</button>
+        <div id="ARTS_ENTERTAINMENT" onClick={this.handleCatChange}>ARTS_ENTERTAINMENT</div>
+        <div id="EDUCATION" onClick={this.handleCatChange}>EDUCATION</div>
+        <div id="FITNESS_RECREATION" onClick={this.handleCatChange}>FITNESS_RECREATION</div>
+        <div id="FOOD_BEVERAGE" onClick={this.handleCatChange}>FOOD_BEVERAGE</div>
+        <div id="HOTEL_LODGING" onClick={this.handleCatChange}>HOTEL_LODGING</div>
+        <div id="MEDICAL_HEALTH" onClick={this.handleCatChange}>MEDICAL_HEALTH</div>
+        <div id="SHOPPING_RETAIL" onClick={this.handleCatChange}>SHOPPING_RETAIL</div>
+        <div id="TRAVEL_TRANSPORTATION" onClick={this.handleCatChange}>TRAVEL_TRANSPORTATION</div>
       </div>
     );
   }
+
+  handleCatChange = (event) => {
+    const newCat = event.target.id;    
+    const categoryIndex = this.state.categories.findIndex(category => {
+        return category === newCat;
+    })
+    if (categoryIndex === -1) {
+      this.setState(prevState => ({
+        categories: [...prevState.categories, newCat]
+      }));
+    } else {
+      const array = this.state.categories;
+      array.splice(categoryIndex, 1);
+      this.setState(prevState => ({
+        categories: array
+      }));
+    }
+  }; //end of cat change
 
   handleLocation = (event) => {
     this.setState({
