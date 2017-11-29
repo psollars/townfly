7e8e0f70edd8f8947cdb37376aa340c9b820b18a
Patrick Sollars <pjsollars@gmail.com>
2017-11-29 14:00:55 -0500

    adds toggling button active class and styles

--- client/src/components/EventSearch.js
+++ client/src/components/EventSearch.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchEvents } from '../actions';
 import moment from 'moment';
-//import _ from 'lodash';
 
 class EventSearch extends Component {
 
@@ -10,7 +9,7 @@ class EventSearch extends Component {
     super(props);
     this.state = {
       location: "",
-      date: [],
+      date: [moment().unix(), moment().add(7, 'days').unix(), 3],
       categories: []
       // distance
     }
@@ -21,9 +20,9 @@ class EventSearch extends Component {
         <div className="EventSearch">
           <input type="text" placeholder="location" onChange={this.handleLocation} />
           <div className="date-params">
-            <button value="1" onClick={this.handleDate}>Today</button>
-            <button value="2" onClick={this.handleDate}>Tomorrow</button>
-            <button value="3" onClick={this.handleDate}>This Week</button>
+            <div className={ this.state.date[2] === 1 ? "radio-button active" : "radio-button"} data-date-param="1" onClick={this.handleDate}>today</div>
+            <div className={ this.state.date[2] === 2 ? "radio-button active" : "radio-button"} data-date-param="2" onClick={this.handleDate}>tomorrow</div>
+            <div className={ this.state.date[2] === 3 ? "radio-button active" : "radio-button"} data-date-param="3" onClick={this.handleDate}>this week</div>
           </div>
           <button onClick={this.handleSubmit}>Fetch Events</button>
         </div>
@@ -37,22 +36,22 @@ class EventSearch extends Component {
   };
 
   handleDate = (event) => {
-    const dateParam = event.target.value;
+    const dateParam = event.target.dataset.dateParam;
     const nowUnix = moment().unix();
     const endOfTodayUnix = moment().endOf("day").unix();
     const endOfTomorrowUnix = moment().endOf("day").add(1, 'days').unix();
     const endOfWeekUnix = moment().add(7, 'days').unix();
     if (dateParam === "1") {
       this.setState({
-        date: [nowUnix, endOfTodayUnix]
+        date: [nowUnix, endOfTodayUnix, 1]
       })
     } else if (dateParam === "2") {
       this.setState({
-        date: [endOfTodayUnix, endOfTomorrowUnix]
+        date: [endOfTodayUnix, endOfTomorrowUnix, 2]
       })
     } else {
       this.setState({
-        date: [nowUnix, endOfWeekUnix]
+        date: [nowUnix, endOfWeekUnix, 3]
       })
     }
   };
