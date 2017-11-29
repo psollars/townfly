3a71ab7efd563dc6d2319271cfc0d05555525efd
Jillane Cook <cookjillane@gmail.com>
2017-11-29 10:27:57 -0500

    moment timestamp on event detail

--- client/src/components/EventDetail.js
+++ client/src/components/EventDetail.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import Moment from 'react-moment';
 import 'moment-timezone';
 
+
+
 class EventDetail extends Component {
 
 
   render() {
     
-    const dateStamp = this.props.event.startTime;
     
+ 
+
   //   const categoryPrint = ()=> {
   //     if (this.props.event.category === true){
   //     <p className= "eventCategory">{this.props.event.category}</p>;
@@ -22,18 +25,16 @@ class EventDetail extends Component {
       <div className ="eventCanvas">
         <div className="EventDetail">
           <p className= "eventTitle">{this.props.event.name}</p>
-          <p className= "eventDate"><Moment>{dateStamp}</Moment></p>
+          <p className= "eventDate"><Moment format="dddd MMMM DD YYYY">{this.props.event.startTime}</Moment></p>
           
           <div className ="imageCard">
             <img className= "eventImage" src = {this.props.event.coverPicture}/>
             <p className= "eventCategory">{this.props.event.category}</p>
-
-            
           </div>
           
           <div className ="eventInfo">
             <div className ="eventInfoCard">
-              <p className= "eventTime"><Moment>{dateStamp}</Moment></p>
+              <p className= "eventTime"><Moment format="LT">{this.props.event.startTime}</Moment></p>
               <p className= "eventLocation">{this.props.event.place.location.street}{this.props.event.place.location.city}{this.props.event.place.location.state}{this.props.event.place.location.zip}</p>
               <p className= "eventDescription">{this.props.event.description}</p>
               <button className ="viewMore">view more</button>
