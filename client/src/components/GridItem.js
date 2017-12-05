679490d4afc24ed0bb0405be572eeb2438e7761f
Sloth I <github@slothwerks.com>
2017-12-05 11:00:14 -0500

    Events > EventGrid > GridItem component flow implemented

--- client/src/components/GridItem.js
+++ client/src/components/GridItem.js
@@ -1,69 +1,97 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import SwipeableViews from 'react-swipeable-views';
-import Card from './Card';
+import Moment from 'react-moment';
+import AddToCalendar from 'react-add-to-calendar';
+import 'moment-timezone';
 
-class EventCards extends Component {
+class GridItem extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeEventIndex: 0,
+      expanded: false,
+      calendarEvent: {
+        title: this.props.event.name,
+        description: this.props.event.description,
+        location: this.props.event.place.name,
+        startTime: this.props.event.startTime,
+        endTime: this.props.event.endTime
+      }
     };
   }
 
   render() {
-    const showEvents = this.props.events.map((event, index) => {
-      return <Card
-                style={Object.assign({}, slideStyles.slide)}
-                key={event.id}
-                event={event}
-                active={index + 1}
-                length={this.props.events.length}
-              />;
-    })
+    const fullAddress = `${this.props.event.place.location.street}, ${this.props.event.place.location.city}, ${this.props.event.place.location.state} ${this.props.event.place.location.zip}`;
+    const directionParams = `origin=${this.props.location.formattedAddress}&destination=${fullAddress}`;
     return (
-      <div className="EventCards">
-          <button className="previousEventButton" onClick={this.previousEvent}>&lt;</button>
-          <button className="nextEventButton" onClick={this.nextEvent}>&gt;</button>
-          <SwipeableViews className="swipeable-views" index={this.state.activeEventIndex} onChangeIndex={this.handleChangeIndex}>
-            {showEvents}
-          </SwipeableViews>
+      <div className="event-list-item">
+        <div className="event-image-container">
+          <div className="event-image-wrapper">
+            <img className="event-image" src={this.props.event.coverPicture}/>
+          </div>
+        </div>
+        <div className="event-info-container">
+          <div className="event-info-card">
+            <p className="event-title">{this.props.event.name}</p>
+            <p className="event-date"><Moment format="dddd, MMMM Do, YYYY">{this.props.event.startTime}</Moment></p>
+            <hr className="divider" />
+            <div className="event-time">
+              <p><Moment format="LT">{this.props.event.startTime}</Moment>{this.props.event.endTime === null ? null : <span> - <Moment format="LT">{this.props.event.endTime}</Moment></span> }</p>
+              <div className="event-sharing">  
+                <a href={`https://www.facebook.com/events/${this.props.event.id}`} target="_blank" rel="nofollow"><i className="fa fa-facebook-square" aria-hidden="true"></i></a>
+                <AddToCalendar event={this.state.calendarEvent} buttonLabel="" buttonTemplate={{"calendar" : "left"}} displayItemIcons={false} listItems={[ { google: 'Google' }, { apple: 'Apple Calendar' }, { outlook: 'Outlook' }, { outlookcom: 'Outlook.com' }, { yahoo: 'Yahoo' } ]} />              
+              </div>
+            </div>
+            <p className="event-venue">{this.props.event.venue.name}</p>
+            <a className="event-location" href={`https://www.google.com/maps/search/?api=1&query=${encodeURI(fullAddress)}`} target="_blank" rel="nofollow">
+              <p>{fullAddress}</p>
+              <i className="fa fa-external-link" aria-hidden="true"></i>
+            </a>
+            <p className={this.state.expanded ? `event-description expanded` : `event-description` }>{this.props.event.description}</p>
+            {this.state.expanded ? 
+            <div className="event-expanded-container">
+              <div className="event-map-container">
+                <div className="event-map">
+                </div>
+                <div className="directions">
+                  <a href={`https://www.google.com/maps/dir/?api=1&${directionParams}&travelmode=driving`}  target="_blank" rel="nofollow"><i className="fa fa-car" aria-hidden="true"></i></a>
+                  <a href={`https://www.google.com/maps/dir/?api=1&${directionParams}&travelmode=walking`}  target="_blank" rel="nofollow"><i className="fa fa-street-view" aria-hidden="true"></i></a>
+                  <a href={`https://www.google.com/maps/dir/?api=1&${directionParams}&travelmode=bicycling`}  target="_blank" rel="nofollow"><i className="fa fa-bicycle" aria-hidden="true"></i></a>
+                  <a href={`https://www.google.com/maps/dir/?api=1&${directionParams}&travelmode=transit`}  target="_blank" rel="nofollow"><i className="fa fa-bus" aria-hidden="true"></i></a>
+                </div>
+              </div>
+            </div>
+            : null }
+            <div className="view-toggle">
+            {this.state.expanded ? 
+              <div onClick={this.handleExpand}>
+                <i className="fa fa-chevron-up" aria-hidden="true"></i>
+                <p>view less</p>
+              </div>
+              : 
+              <div onClick={this.handleExpand}>
+                <p>view more</p>
+                <i className="fa fa-chevron-down" aria-hidden="true"></i>
+              </div>
+            }
+            </div>
+          </div>  
+        </div>
       </div>
     );
   }
 
-  nextEvent = (event) => {
-    event.preventDefault();
-    if (this.state.activeEventIndex === this.state.eventsToDisplay.length - 1) {
-      return;
-    } else {
-      let newIndex = this.state.activeEventIndex + 1;
-      this.setState({
-        activeEventIndex: newIndex
-      })
-    }
-  }
-
-  previousEvent = (event) => {
-    event.preventDefault();
-    if (this.state.activeEventIndex === 0) {
-      return;
-    } else {
-      let newIndex = this.state.activeEventIndex - 1;
-      this.setState({
-        activeEventIndex: newIndex
-      })
-    }
-  }
+  handleExpand = () => {
+    this.setState({
+      expanded: !this.state.expanded
+    });
+  };
 
 }
 
-const slideStyles = {
-  slide: {
-    padding: 15,
-    minHeight: 100,
-    color: "#fff"
-  }
+function mapStateToProps(state) {
+  return {
+    location: state.location
+  };
 }
 
-export default EventCards;
+export default connect(mapStateToProps)(GridItem);
